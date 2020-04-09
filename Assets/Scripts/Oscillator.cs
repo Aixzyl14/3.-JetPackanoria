b43063c03f2c4e7ebd265540892400ccb4f2d6df
Aixcel100 <aixzyl@yahoo.co.uk>
2020-04-09 13:38:32 +0100

    Revert "Level 1 - 4 Polished"
    This reverts commit 6fe04a4962e414d5fdb84f9db7aff89601cf094d.

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -8,10 +8,8 @@ public class Oscillator : MonoBehaviour
     [Range(0, 100)]
     [SerializeField] float period = 2f;
     [SerializeField] Vector3 MovementVector = new Vector3(10f, 10f, 10f);
-
     float movementFactor; // 0 for not moved, 1 for fully moved
     Vector3 startingPos; // must be stored for absolute  movement
-
 
     private void Start()
     {
@@ -21,15 +19,15 @@ public class Oscillator : MonoBehaviour
     private void Update()
     {
         if (period <= Mathf.Epsilon) { print("Period cannot be 0"); return; }
-
+        
             float cycles = Time.time / period; //grows continually from zero
 
             const float tau = Mathf.PI * 2; //2 pi for full cycle
             float rawSineWave = Mathf.Sin(cycles * tau);
-             
-            movementFactor = rawSineWave / 2f + 0.5f; //half amplitude but then need to add 0.5 to get a range of -1 to 1 treat as double
-            Vector3 offsetMov = MovementVector * movementFactor;
-            transform.position = startingPos + offsetMov;
 
+            movementFactor = rawSineWave / 2f + 0.5f; //half amplitude but then need to add 0.5 to get a range of -1 to 1 treat as double
+            Vector3 offset = MovementVector * movementFactor;
+            transform.position = startingPos + offset;
+     
     }
 }
